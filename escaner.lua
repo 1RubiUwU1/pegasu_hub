95ffeec17b8f43bbbaaf74b53b21f70801bc9046
hola <110574193+1RubiUwU1@users.noreply.github.com>
2022-08-22 19:14:14 -0500

    Add files via upload

---
+++ escaner.lua
@@ -0,0 +1,12 @@
+---SCANNER
+local ESP_Boxes = false
+local ESP_Names = false 
+local ESP_Health = false
+local ESP_Enabled = false
+local ESP_Chams = false
+local ESP_Chams2 = false
+local ESP_Chams_Outline = false
+local ESP_Main_Color = Color3.new(1,0,0)
+local ESP_Chams_Color = Color3.new(0,0,0)
+local ESP_Chams_Color2 = Color3.new(0,0,0)
+local ESP_Chams_Outline_Color = Color3.new(1,0,0)
