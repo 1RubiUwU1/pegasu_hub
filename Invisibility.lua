72e608d717b057046c13a0fbb34d941d63bb8b33
hola <110574193+1RubiUwU1@users.noreply.github.com>
2022-08-25 21:03:54 -0500

    Add files via upload

---
+++ Invisibility.lua
@@ -0,0 +1,37 @@
+-----[Made By:Moin]---------------------
+
+DisableRobloxOnlyOutput = true
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Made By";
+        Text = "Joseph";
+        Text = "Plss sub to Yt_Moin"
+        })
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Joseph";
+        Text = "Press = "J" ";
+        })
+
+local plr = game:GetService("Players").LocalPlayer
+local mouse = plr:GetMouse()
+
+
+mouse.KeyDown:connect(function(key)
+    if key == "j" then
+local plr = game.Players.LocalPlayer
+game.Workspace.Live[plr.Name].Head["face"]:Destroy()
+wait(0.2)
+game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack["Flash Strike"])
+game.Players.LocalPlayer.Character["Flash Strike"].Activator.Animation:Destroy()
+game.Players.LocalPlayer.Character["Flash Strike"]:Activate()
+wait()
+local plr = game.Players.LocalPlayer
+game.Workspace.Live[plr.Name]["RebirthWings"]:Destroy()
+wait()
+local plr = game.Players.LocalPlayer
+game.Workspace.Live[plr.Name].HumanoidRootPart["PowerLevel"]:Destroy()
+end
+end)
+
+
+
+
